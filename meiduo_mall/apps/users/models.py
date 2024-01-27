258dd3a4a910ccd8deb2ae8fd111624c5f70d661
tuanzi <763302385@qq.com>
2024-01-27 17:20:31 +0800

    定义用户模型类

--- meiduo_mall/apps/users/models.py
+++ meiduo_mall/apps/users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+# django自带一个用户模型
+# 这个用户模型,有密码的加密,和密码的验证
+class User(AbstractUser):
+    mobile = models.CharField(max_length=11,unique=True)
+
+    class Meta:
+        db_tabel = 'tb_users'
+        verbose_name = '用户管理'
+        verbose_name_plural = verbose_name
