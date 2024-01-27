3d20ce47e17b957825a34a70fc39b6115645ce65
tuanzi <763302385@qq.com>
2024-01-27 23:33:43 +0800

    自定义转换器

--- meiduo_mall/apps/users/urls.py
+++ meiduo_mall/apps/users/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from apps.users.views import UsernameCountView
 
 urlpatterns = [
-    path('usernames/<username>/count/',UsernameCountView.as_view()),
+    path('usernames/<username:username>/count/',UsernameCountView.as_view()),
 ]
--- meiduo_mall/apps/users/views.py
+++ meiduo_mall/apps/users/views.py
@@ -1,3 +1,5 @@
+import re
+
 from django.shortcuts import render
 from django.views import View
 from django.http import JsonResponse
@@ -8,7 +10,9 @@ from apps.users.models import User
 class UsernameCountView(View):
 
     def get(self,request,username):
-        # 1.接收用户名
+        # 1.接收用户名，对这个用户名进行判断
+        # if not re.match('[a-zA-Z0-9_-]{5,20}',username):
+        #     return JsonResponse({'code':200,'errmsg':'用户名不满足规范'})
         # 2.根据用户名查询数据库
         count = User.objects.filter(username=username).count()
         # 3.返回响应
