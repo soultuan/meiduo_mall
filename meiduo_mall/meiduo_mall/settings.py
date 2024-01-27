4e586225506ddca38c53bf7d5fc6cc5407d73c2b
tuanzi <763302385@qq.com>
2024-01-27 11:11:48 +0800

    Mysql配置

--- meiduo_mall/meiduo_mall/settings.py
+++ meiduo_mall/meiduo_mall/settings.py
@@ -75,12 +75,8 @@ WSGI_APPLICATION = 'meiduo_mall.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'HOST':'127.0.0.1',
-        'PORT':3306,
-        'USER':'root',
-        'PASSWORD':'xyl20001122',
-        'NAME':'meiduo_mall',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
