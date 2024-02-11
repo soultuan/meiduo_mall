fe7b58c3417139a40e069bb25cae00fe298eed27
tuanzi <763302385@qq.com>
2024-02-12 00:04:33 +0800

    省份数据的获取

---
+++ meiduo_mall/apps/areas/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from apps.areas.views import AreaView
+
+urlpatterns = [
+    path('areas/',AreaView.as_view()),
+]
--- meiduo_mall/apps/areas/views.py
+++ meiduo_mall/apps/areas/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
+from django.views import View
+from apps.areas.models import Area
+from django.http import JsonResponse
 # Create your views here.
+class AreaView(View):
+    def get(self,request):
+        # 1.查询省份信息
+        # 返回的province是查询结果集->queryset格式
+        provinces = Area.objects.filter(parent=None)
+        # 2.将对象转换成字段数据
+        province_list = []
+        for province in provinces:
+            province_list.append({
+                'id':province.id,
+                'name':province.name
+            })
+        # 3.返回响应
+        return JsonResponse({'code':0,'errmsg':'ok','province_list':province_list})
